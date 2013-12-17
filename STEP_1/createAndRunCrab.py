59ead0c890a24c731b2dbcc8f2ac1623fbfef329
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-12-17 14:41:45 +0100

    Updated STEP 1 scripts

--- STEP_1/createAndRunCrab.py
+++ STEP_1/createAndRunCrab.py
@@ -37,7 +37,7 @@ for dataset_info in datasets:
   dataset_name = dataset_info[1];
 
   ui_working_dir = ("crab_%s") % (dataset_name)
-  publish_name = "%s_%s_START53_V19-GEN-SIM" % (dataset_name, d)
+  publish_name = "%s_%s_START53_V19-GEN-SIM-RAW" % (dataset_name, d)
   output_file = "crab_%s.cfg" % (dataset_name)
   python_file = "%s_cff.py" % (dataset_name)
 
@@ -46,12 +46,12 @@ for dataset_info in datasets:
   print("")
 
   # Execute cmsDriver.py
-  args = ["cmsDriver.py", "step1", "--filein", "file:dummy.root", "--fileout", "file:output_GEN-SIM.root", "--mc", "--eventcontent", "RAWSIM", "--datatier", "GEN-SIM-RAW", "--conditions", "START53_V19::All", "--beamspot", "Realistic8TeVCollision", "--pileup", "2012_Summer_50ns_PoissonOOTPU", "--pileup_input", "dbs:/MinBias_TuneZ2star_8TeV-pythia6/Summer12-START50_V13-v3/GEN-SIM", "--step", "SIM,DIGI,L1,DIGI2RAW,HLT:7E33v2", "--no_exec", "--python_file", python_file, "-n", "5"]
+  args = ["cmsDriver.py", "step1", "--filein", "file:dummy.root", "--fileout", "file:output_GEN-SIM-RAW.root", "--mc", "--eventcontent", "RAWSIM", "--datatier", "GEN-SIM-RAW", "--conditions", "START53_V19::All", "--beamspot", "Realistic8TeVCollision", "--pileup", "2012_Summer_50ns_PoissonOOTPU", "--pileup_input", "dbs:/MinBias_TuneZ2star_8TeV-pythia6/Summer12-START50_V13-v3/GEN-SIM", "--step", "SIM,DIGI,L1,DIGI2RAW,HLT:7E33v2", "--no_exec", "--python_file", python_file, "-n", "5"]
   with open("/dev/null", "w") as f:
     subprocess.call(args, stdout=f)
 
   # Create crab template
-  os.system("sed -e \"s#@datasetpath@#%(datasetpath)s#\" -e \"s#@pset@#%(pset)s#\" -e \"s#@events@#%(events)s#\" -e \"s#@working_dir@#%(working_dir)s#\" -e \"s#@output_dir@#%(output_dir)s#\" -e \"s#@publish_name@#%(publish_name)s#\" -e \"s#@event_per_job@#%(event_per_job)s#\" crab.template > %(output)s" % {"pset": python_file, "output": output_file, "working_dir": ui_working_dir, "events": -1, "output_dir": "NOT_USED", "publish_name": publish_name, "event_per_job": 1300, "datasetpath": dataset_files})
+  os.system("sed -e \"s#@datasetpath@#%(datasetpath)s#\" -e \"s#@pset@#%(pset)s#\" -e \"s#@events@#%(events)s#\" -e \"s#@working_dir@#%(working_dir)s#\" -e \"s#@output_dir@#%(output_dir)s#\" -e \"s#@publish_name@#%(publish_name)s#\" -e \"s#@event_per_job@#%(event_per_job)s#\" crab.template > %(output)s" % {"pset": python_file, "output": output_file, "working_dir": ui_working_dir, "events": -1, "output_dir": "NOT_USED", "publish_name": publish_name, "event_per_job": 550, "datasetpath": dataset_files})
 
   if options.create:
     cmd = "crab -create -cfg %s" % (output_file)
