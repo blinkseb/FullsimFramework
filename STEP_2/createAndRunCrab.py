806a9b3e41ef2c200c6fccd23313e57fa6c800b6
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-12-28 12:33:31 +0100

    Only run locally for step 2

--- STEP_2/createAndRunCrab.py
+++ STEP_2/createAndRunCrab.py
@@ -45,13 +45,13 @@ for dataset_info in datasets:
   print("")
 
   # Execute cmsDriver.py
-  args = ["cmsDriver.py", "step2", "--filein", "file:dummy.root", "--fileout", "file:output_GEN-SIM-RAW.root", "--mc", "--eventcontent", "RAWSIM", "--datatier", "GEN-SIM-RAW", "--conditions", "START53_V19::All", "--beamspot", "Realistic8TeVCollision", "--pileup", "2012_Summer_50ns_PoissonOOTPU", "--pileup_input", "dbs:/MinBias_TuneZ2star_8TeV-pythia6/Summer12-START50_V13-v3/GEN-SIM", "--step", "DIGI,L1,DIGI2RAW,HLT:7E33v2", "--no_exec", "--python_file", python_file, "-n", "5"]
+  args = ["cmsDriver.py", "step2", "--filein", "file:dummy.root", "--fileout", "file:output_GEN-SIM-RAW.root", "--mc", "--eventcontent", "RAWSIM", "--datatier", "GEN-SIM-RAW", "--conditions", "START53_V19::All", "--beamspot", "Realistic8TeVCollision", "--pileup", "2012_Summer_50ns_PoissonOOTPU", "--pileup_input", "dbs:/MinBias_TuneZ2star_8TeV-pythia6/Summer12-START50_V13-v3/GEN-SIM", "--step", "DIGI,L1,DIGI2RAW,HLT:7E33v2", "--no_exec", "--python_file", python_file, "-n", "30"]
   # args = ["cmsDriver.py", "step2", "--filein", "file:dummy.root", "--fileout", "file:output_GEN-SIM-RAW-RECO.root", "--mc", "--eventcontent", "AODSIM", "--datatier", "AODSIM", "--conditions", "START53_V19::All", "--beamspot", "Realistic8TeVCollision", "--pileup", "2012_Summer_50ns_PoissonOOTPU", "--pileup_input", "dbs:/MinBias_TuneZ2star_8TeV-pythia6/Summer12-START50_V13-v3/GEN-SIM", "--step", "DIGI,L1,DIGI2RAW,HLT:7E33v2,RAW2DIGI,L1Reco,RECO", "--no_exec", "--python_file", python_file, "-n", "5"]
   with open("/dev/null", "w") as f:
     subprocess.call(args, stdout=f)
 
   # Create crab template
-  os.system("sed -e \"s#@datasetpath@#%(datasetpath)s#\" -e \"s#@pset@#%(pset)s#\" -e \"s#@events@#%(events)s#\" -e \"s#@working_dir@#%(working_dir)s#\" -e \"s#@output_dir@#%(output_dir)s#\" -e \"s#@publish_name@#%(publish_name)s#\" -e \"s#@event_per_job@#%(event_per_job)s#\" crab.template > %(output)s" % {"pset": python_file, "output": output_file, "working_dir": ui_working_dir, "events": -1, "output_dir": "NOT_USED", "publish_name": publish_name, "event_per_job": 3000, "datasetpath": dataset_files})
+  os.system("sed -e \"s#@datasetpath@#%(datasetpath)s#\" -e \"s#@pset@#%(pset)s#\" -e \"s#@events@#%(events)s#\" -e \"s#@working_dir@#%(working_dir)s#\" -e \"s#@output_dir@#%(output_dir)s#\" -e \"s#@publish_name@#%(publish_name)s#\" -e \"s#@event_per_job@#%(event_per_job)s#\" crab.template > %(output)s" % {"pset": python_file, "output": output_file, "working_dir": ui_working_dir, "events": -1, "output_dir": "NOT_USED", "publish_name": publish_name, "event_per_job": 4300, "datasetpath": dataset_files})
 
   if options.create:
     cmd = "crab -create -cfg %s" % (output_file)
@@ -59,15 +59,15 @@ for dataset_info in datasets:
 
     print "Modifying list of input files..."
     # Edit arguments.xml file to hardcode input files. Allow to run at CERN!
-    argFile = os.path.join(ui_working_dir, 'share', 'arguments.xml')
-    dom = xml.dom.minidom.parse(argFile)
-    for elem in dom.getElementsByTagName("Job"):
-      inputFiles = str(elem.getAttribute('InputFiles'))
-      inputFileNames = inputFiles.split(',')
-      inputFileNames = ["root://ccxrpli001.in2p3.fr:1094/" + inputFileName if not "root://" in inputFileName else inputFileName for inputFileName in inputFileNames]
-      elem.setAttribute('InputFiles', ",".join(inputFileNames))
-    with open(argFile, 'w') as f:
-      f.write(dom.toxml())
+    #argFile = os.path.join(ui_working_dir, 'share', 'arguments.xml')
+    #dom = xml.dom.minidom.parse(argFile)
+    #for elem in dom.getElementsByTagName("Job"):
+      #inputFiles = str(elem.getAttribute('InputFiles'))
+      #inputFileNames = inputFiles.split(',')
+      #inputFileNames = ["root://ccxrpli001.in2p3.fr:1094/" + inputFileName if not "root://" in inputFileName else inputFileName for inputFileName in inputFileNames]
+      #elem.setAttribute('InputFiles', ",".join(inputFileNames))
+    #with open(argFile, 'w') as f:
+      #f.write(dom.toxml())
     print "Done"
 
   if options.run:
